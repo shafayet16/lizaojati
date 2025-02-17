da63dae49a3ab663827ced7af284b5e8f0f9553d
shafayet16 <hirasan893@gmail.com>
2025-02-17 05:28:16 +0000

    Feb 17, 2025, 11:28 AM

--- lizaojati/webdev/Js/HUXN/num.js
+++ lizaojati/webdev/Js/HUXN/num.js
@@ -1,2 +1,9 @@
 let favNum = 7;
 let seconfFavNum = 9;
+let sum = favNum + seconfFavNum;
+let subract = favNum - seconfFavNum;
+let multiply = favNum * seconfFavNum;
+let divide = favNum / seconfFavNum;
+let modulus = favNum % seconfFavNum;
+let sqaure = favNum ** seconfFavNum;
+console.log(sum, subract, multiply, divide, modulus, sqaure);

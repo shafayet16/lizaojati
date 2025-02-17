7c6e4ce448f6ec6e239a8c2dc599d935eb29a9eb
shafayet16 <hirasan893@gmail.com>
2025-02-17 05:31:54 +0000

    Feb 17, 2025, 11:31 AM

---
+++ lizaojati/webdev/Js/HUXN/boolean.js
@@ -0,0 +1,9 @@
+let isJsHard = false;
+let isJsEasy = true;
+console.log(isJsHard, isJsEasy);
+let isJsFun = true;
+let isJsBoring = false;
+console.log(isJsFun, isJsBoring);
+let isJsInteresting = true;
+let isJsUninteresting = false;
+console.log(isJsInteresting, isJsUninteresting);
